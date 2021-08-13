9413b0386d0f103b70892de8762cc703509afa4a
amittnt <84276312+amittnt@users.noreply.github.com>
2021-08-13 08:26:46 +0530

    update

--- js/32care.js
+++ js/32care.js
@@ -54,13 +54,19 @@ $('.close-booking').on('click', function(e) {
 
 });
 
-$('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('New message to ' + recipient)
-  modal.find('.modal-body input').val(recipient)
-})
+/*$('.primary-btn').on('click', function(e) {
+			$(".primary-btn span").removeClass("fa fa-caret-right fadeOut")
+			$(".primary-btn").removeClass("primary-btn")
+			
+
+});*/
+
+function myFunction() {
+  document.getElementById("mySubmit").value = "Submitted";
+}
+
+
+
+
+
 
